2adda6933d67cba2e8c31fd1c0c8c67465d61634
Victor Gabriel <vcgabriel38@gmail.com>
2024-02-05 16:01:16 -0300

    feat: create input styles

---
+++ src/components/Input/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width:100%;
+    max-width:300px;
+    height:30px;
+    border-bottom: 1px solid #fff;
+    margin-bottom: 20px;
+    display:flex;
+    align-items: center;
+`
+export const IconContainer = styled.div`
+    margin-right:10px;
+`
+export const InputText = styled.input`
+    background-color: transparent;
+    color: #fff;
+    border: 0;
+    height: 30px;
+    width: 100%;
+`
