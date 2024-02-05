344cd0dd2ef44ec69782526d951e176eb3f9c397
Victor Gabriel <vcgabriel38@gmail.com>
2024-02-05 16:02:46 -0300

    feat: create login page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,9 +1,49 @@
-import {Link} from 'react-router-dom';
+import React from "react";
+import {MdEmail, MdLock} from 'react-icons/md';
+import CustomButton from "../../components/Button/index";
+
+import { Header } from "../../components/Header/index";
+import { Input } from "../../components/Input/index";
+import {
+  Container,
+  Column,
+  Row,
+  Wrapper,
+  Title,
+  TitleLogin,
+  SubtitleLogin,
+  ForgotText,
+  CreateText,
+} from "./styles";
+
 const Login = () => {
-    return (<>
-        <h1>Login</h1>
-        <Link to="/">Voltar para Home</Link>
+  return (
+    <>
+      <Header />
+      <Container>
+        <Column>
+          <Title>
+            A plataforma para você aprender com experts, dominar as principais
+            tecnologias e entrar mais rápido nas empresas mais desejadas.
+          </Title>
+        </Column>
+        <Column>
+          <Wrapper>
+            <TitleLogin>Faça seu Cadastro</TitleLogin>
+            <SubtitleLogin>Faça seu login e make the change.</SubtitleLogin>
+            <form action="">
+              <Input placeholder="Email" type="email" leftIcon={<MdEmail/>} />
+              <Input placeholder="Senha" type="password" leftIcon={<MdLock/>} />
+            </form>
+            <CustomButton title="Entrar" variant="secondary" />
+            <Row>
+              <ForgotText>Esqueceu a Senha?</ForgotText>
+              <CreateText>Criar conta</CreateText>
+            </Row>
+          </Wrapper>
+        </Column>
+      </Container>
     </>
-    )
-}
-export default Login 
+  );
+};
+export default Login;
