d62009054dbcf5d97b00be3f06bcf9a84d44699e
Victor Gabriel <vcgabriel38@gmail.com>
2024-02-06 17:50:14 -0300

    feat: implement of use navigate

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,35 +1,45 @@
-import { Link } from 'react-router-dom';
-import CustomButton from '../../components/Button/index';
-import bannerImage from '../../assets/banner.png';
-import { Header } from '../../components/Header/index';
-import { Container, TextContent, Title, TitleHighLight } from './styles';
-
+import CustomButton from "../../components/Button/index";
+import bannerImage from "../../assets/banner.png";
+import { Header } from "../../components/Header/index";
+import { Container, TextContent, Title, TitleHighLight } from "./styles";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-    return (
-        <>
-        <Header/>
-            <Container>
-                <div>
-                    <Title>
-                        <TitleHighLight>
-                        Implemente 
-                        <br />
-                        </TitleHighLight>
-                        o seu futuro global agora!
-                    </Title>
-                    <TextContent>
-                        Domine as tecnologias utilizadas pelas empresas mais inovadoras do mundo e encare seu novo desafio profissional, evoluindo em comunidade com os melhores experts.
-                    </TextContent>
-                    <Link to={"/login"}>
-                        <CustomButton title="Começar agora" variant='secondary' onClick={() => null} />
-                    </Link>
-                </div>
-                <div>
-                    <img src={bannerImage} alt="Imagem Principal" />
-                </div>
-            </Container>
-        </>
-    )
-}
-export default Home 
+  const navigate = useNavigate();
+
+  const handleClickSignIn = () => {
+    navigate("/login");
+  };
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <div>
+          <Title>
+            <TitleHighLight>
+              Implemente
+              <br />
+            </TitleHighLight>
+            o seu futuro global agora!
+          </Title>
+          <TextContent>
+            Domine as tecnologias utilizadas pelas empresas mais inovadoras do
+            mundo e encare seu novo desafio profissional, evoluindo em
+            comunidade com os melhores experts.
+          </TextContent>
+          <CustomButton
+            title="Começar agora"
+            variant="secondary"
+            onClick={handleClickSignIn}
+            type="button"
+          />
+        </div>
+        <div>
+          <img src={bannerImage} alt="Imagem Principal" />
+        </div>
+      </Container>
+    </>
+  );
+};
+export default Home;
