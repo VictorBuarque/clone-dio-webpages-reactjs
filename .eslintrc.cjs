1a699cd664e2b521a00e2f37e4ce060a12419b1c
Victor Buarque <vcgabriel38@gmail.com>
2024-03-12 16:15:27 -0300

    chore: add rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,10 +11,8 @@ module.exports = {
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+  "rules": {
+    "singleQuote": 2,
+    "semi": [2, "never"]
+  }
 }
