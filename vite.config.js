2ba563fc18fd43a7962f2325715970de89dbf7d2
Victor Buarque <vcgabriel38@gmail.com>
2024-03-22 14:59:08 -0300

    chore: add config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-			include: /\.(jsx|tsx)$/,
-			babel: {
-				plugins: ['styled-components'],
-				babelrc: false,
-				configFile: false,
-			},
-		})],
-})
+  resolve: {
+    extensions: [".js", ".ts", ".jsx", ".tsx"],
+  },
+  plugins: [
+    react({
+      include: /\.(jsx|tsx)$/,
+      babel: {
+        plugins: ["styled-components"],
+        babelrc: false,
+        configFile: false,
+      },
+    }),
+  ],
+});
