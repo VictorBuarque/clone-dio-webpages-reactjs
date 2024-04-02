d890c1548f5914fe5c1c65f3ff3cf0c3327c7861
Victor Buarque <vcgabriel38@gmail.com>
2024-04-02 13:02:36 -0300

    feat: create auth context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
-import Login from './pages/login';
-import Feed from './pages/feed';
-import Register from './pages/register';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Feed from "./pages/feed";
+import Register from "./pages/register";
+import { AuthContextProvider } from "./context/auth";
 
+// when we work with router everything need to be inside the router
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/feed" element={<Feed />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </AuthContextProvider>
     </Router>
   );
 }
